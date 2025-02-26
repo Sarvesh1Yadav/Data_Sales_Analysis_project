43570739b2178e3add3381424137e748dd3ba8c2
Sarvesh Yadav <116245806+Sarvesh1Yadav@users.noreply.github.com>
2025-02-26 14:56:59 +0530

    Update power.py

--- power.py
+++ power.py
@@ -7,6 +7,6 @@ image_file = r"image/power_page-0001.jpg"  # Use raw string
 
 # Check if file exists before displaying
 if os.path.exists(image_file):
-    st.image(image_file, caption="Power BI Report Visualization", use_column_width=True)
+    st.image(image_file, caption="Power BI Report Visualization", use_container_width=True)
 else:
     st.error(f"Error: File '{image_file}' not found!")
